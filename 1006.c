2bcee3070632eeb63e8dc1bafd799f3149b33c8d
Willians Leite <130706713+wleiteeee@users.noreply.github.com>
2024-03-12 13:34:31 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  double A, B, C, media;
+
+  scanf("%lf", &A);
+
+  scanf("%lf", &B);
+
+  scanf("%lf", &C);
+
+  media = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+  printf("MEDIA = %.1lf\n", media);
+  
+  return 0;
+}
