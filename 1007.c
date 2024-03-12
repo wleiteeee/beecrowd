ba16483d3ce9092952212208d606c726968ad117
Willians Leite <130706713+wleiteeee@users.noreply.github.com>
2024-03-12 13:34:58 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int A, B, C, D, dif;
+
+  scanf("%d", &A);
+
+  scanf("%d", &B);
+
+  scanf("%d", &C);
+
+  scanf("%d", &D);
+
+  dif = (A * B - C * D);
+
+  printf("DIFERENCA = %d\n", dif);
+  
+  return 0;
+}
